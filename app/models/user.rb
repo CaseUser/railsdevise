7b91786837cc9418389affc7aa4e00ee4d4e2854
CaseUser <dear_jump@yahoo.com>
2017-04-28 07:06:36 -0400

    inital commit

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :lockable, :zxcvbnable
+		 
+ def send_devise_notification(notification, *args)
+    if Rails.env.production?
+      # No worker process in production to handle scheduled tasks
+      devise_mailer.send(notification, self, *args).deliver_now
+    else
+      devise_mailer.send(notification, self, *args).deliver_later
+    end
+  end
+  
+end
